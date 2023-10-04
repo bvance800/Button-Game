cf7d27a9a40e642f0ab6a745a90de9a0bfd21238
Brandon Vance <brandonv@qualtrics.com>
2023-10-04 15:18:38 -0600

    random changes

--- server/src/games/game-directory.js
+++ server/src/games/game-directory.js
@@ -1,5 +1,19 @@
 import { info as nameGame } from './name-game/index.js';
 
-export const directory = [
+const directory = [
   nameGame
 ]
+
+function getRegisteredGame(gameId){
+  let foundGameIndex = -1;
+  foundGameIndex = directory.findIndex((gameInfo) => {
+    return gameInfo.id === gameId
+  })
+  if(foundGameIndex < 0){
+    console.log("sorry no game type:", gameId)
+    return null;
+  }
+  return directory[foundGameIndex];
+}
+
+export { directory, getRegisteredGame }
